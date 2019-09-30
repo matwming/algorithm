aa21ff1aa6ee4a2e547b683bad49d5799491f075
Mat <mat.w@bmyg.com.au>
2019-09-30 10:53:27 +1000

    solved sumOfTow problem

--- Array/sumOfTwo.js
+++ Array/sumOfTwo.js
@@ -7,19 +7,48 @@
 const nums = [2, 7, 11, 15];
 const target = 9; //should return [0,1]
 
-const twoSum = array => {
- const copiedNums = [...array];
- copiedNums.sort((a, b) => a - b);
- let bigNum = [];
- nums.forEach((el, index) => {
-  if (el > target) {
-   bigNum.push(el);
-  }
- });
- let bigNumIndex = copiedNums.findIndex((el, index) => {
-  el === bigNum[0];
- });
- let arrayArea = copiedNums.slice(0, bigNumIndex - 1);
- console.log(arrayArea);
+/**
+|--------------------------------------------------
+| Method 1:
+|--------------------------------------------------
+*/
+const removeDuplicate = array => {
+  array.sort((a, b) => a - b);
+  array.forEach((el, index) => {
+    if (array[index] === array[index + 1]) {
+      array.splice(index, 1);
+    }
+  });
+  return array;
 };
-twoSum(nums);
+const twoSum = (nums, target) => {
+  let first = "";
+  let sortNums = removeDuplicate([...nums]);
+  for (let i = 0; i < sortNums.length; i++) {
+    if (nums.includes(target - sortNums[i])) {
+      first = sortNums[i];
+      break;
+    }
+  }
+  let second = target - first;
+  let secondIndex = nums.lastIndexOf(second);
+  let firstIndex = nums.findIndex(el => el === first);
+
+  return [firstIndex, secondIndex];
+};
+console.log(twoSum(nums, target));
+/**
+|--------------------------------------------------
+| Method 2:
+|--------------------------------------------------
+*/
+const twoSum_v2 = function(nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const otherIndex = map.get(target - nums[i]);
+    if (otherIndex !== undefined) return [otherIndex, i];
+    map.set(nums[i], i);
+    console.log("map", map);
+  }
+};
+twoSum_v2(nums, target);
